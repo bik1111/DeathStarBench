bdf47d6a5f0f7aa8f72d83674eed235271d8ea16
bik1111 <bik1111@khu.ac.kr>
2024-03-20 19:30:50 +0900

    fix: resource limst/requests imagepullPolicy

--- hotelReservation/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
+++ hotelReservation/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
@@ -2,32 +2,32 @@ local socket = require("socket")
 math.randomseed(socket.gettime()*1000)
 math.random(); math.random(); math.random()
 
-local url = "http://localhost:5000"
+-- local url = "http://localhost:5000"
 
 local function get_user()
   local id = math.random(0, 500)
   local user_name = "Cornell_" .. tostring(id)
   local pass_word = ""
-  for i = 0, 9, 1 do 
+  for i = 0, 9, 1 do
     pass_word = pass_word .. tostring(id)
   end
   return user_name, pass_word
 end
 
-local function search_hotel() 
+local function search_hotel()
   local in_date = math.random(9, 23)
   local out_date = math.random(in_date + 1, 24)
 
   local in_date_str = tostring(in_date)
   if in_date <= 9 then
-    in_date_str = "2015-04-0" .. in_date_str 
+    in_date_str = "2015-04-0" .. in_date_str
   else
     in_date_str = "2015-04-" .. in_date_str
   end
 
   local out_date_str = tostring(out_date)
   if out_date <= 9 then
-    out_date_str = "2015-04-0" .. out_date_str 
+    out_date_str = "2015-04-0" .. out_date_str
   else
     out_date_str = "2015-04-" .. out_date_str
   end
@@ -36,7 +36,7 @@ local function search_hotel()
   local lon = -122.095 + (math.random(0, 325) - 157.0)/1000.0
 
   local method = "GET"
-  local path = url .. "/hotels?inDate=" .. in_date_str .. 
+  local path = "http://10.244.2.46:5000/hotels?inDate=" .. in_date_str ..
     "&outDate=" .. out_date_str .. "&lat=" .. tostring(lat) .. "&lon=" .. tostring(lon)
 
   local headers = {}
@@ -59,7 +59,7 @@ local function recommend()
   local lon = -122.095 + (math.random(0, 325) - 157.0)/1000.0
 
   local method = "GET"
-  local path = url .. "/recommendations?require=" .. req_param .. 
+  local path = "http://10.244.2.46:5000/recommendations?require=" .. req_param ..
     "&lat=" .. tostring(lat) .. "&lon=" .. tostring(lon)
   local headers = {}
   -- headers["Content-Type"] = "application/x-www-form-urlencoded"
@@ -72,14 +72,14 @@ local function reserve()
 
   local in_date_str = tostring(in_date)
   if in_date <= 9 then
-    in_date_str = "2015-04-0" .. in_date_str 
+    in_date_str = "2015-04-0" .. in_date_str
   else
     in_date_str = "2015-04-" .. in_date_str
   end
 
   local out_date_str = tostring(out_date)
   if out_date <= 9 then
-    out_date_str = "2015-04-0" .. out_date_str 
+    out_date_str = "2015-04-0" .. out_date_str
   else
     out_date_str = "2015-04-" .. out_date_str
   end
@@ -91,7 +91,7 @@ local function reserve()
   local num_room = "1"
 
   local method = "POST"
-  local path = url .. "/reservation?inDate=" .. in_date_str .. 
+  local path = "http://10.244.2.46:5000/reservation?inDate=" .. in_date_str ..
     "&outDate=" .. out_date_str .. "&lat=" .. tostring(lat) .. "&lon=" .. tostring(lon) ..
     "&hotelId=" .. hotel_id .. "&customerName=" .. cust_name .. "&username=" .. user_id ..
     "&password=" .. password .. "&number=" .. num_room
@@ -102,8 +102,8 @@ end
 
 local function user_login()
   local user_name, password = get_user()
-  local method = "POST"
-  local path = url .. "/user?username=" .. user_name .. "&password=" .. password
+  local method = "GET"
+  local path =  "http://10.244.2.46:5000/user?username=" .. user_name .. "&password=" .. password
   local headers = {}
   -- headers["Content-Type"] = "application/x-www-form-urlencoded"
   return wrk.format(method, path, headers, nil)
@@ -123,7 +123,7 @@ request = function()
     return recommend(url)
   elseif coin < search_ratio + recommend_ratio + user_ratio then
     return user_login(url)
-  else 
+  else
     return reserve(url)
   end
 end
